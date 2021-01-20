f7488778b3dd2200dd1acfbd4d8c8738f5e38a68
zwf <zwf19980423@gmail.com>
2021-01-21 00:30:08 +0800

    footer,header,react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Hello</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
