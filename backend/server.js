c0459563a478547b544b2a66eaabb342a95035c6
zwf <zwf19980423@gmail.com>
2021-01-21 20:50:00 +0800

    colors and mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -16,5 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} MODE on ${PORT} `)
+  console.log(
+    `server running in ${process.env.NODE_ENV} MODE on ${PORT} `.yellow.bold
+  )
 );
