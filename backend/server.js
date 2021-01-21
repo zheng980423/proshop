fb34f0c022c845b64534ca2d9d9c91eb085670d8
zwf <zwf19980423@gmail.com>
2021-01-21 11:54:19 +0800

    commonjs->esmodule

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API is running.'));
+app.get('/', (req, res) => res.send('API is running...'));
 app.get('/api/products', (req, res) => res.json(products));
 app.get('/api/products/:id', (req, res) => {
   const product = products.find(p => p._id === req.params.id);
