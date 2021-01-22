b1425a6d3e5796080f78fe04e27cd43265c8ef9d
zwf <zwf19980423@gmail.com>
2021-01-23 00:13:03 +0800

    productdetailreducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
-const reducer = combineReducers({ productList: productListReducer });
+import {
+  productListReducer,
+  productDetailsListReducer,
+} from './reducers/productReducers';
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsListReducer,
+});
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
