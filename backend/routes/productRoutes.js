118325abb5ee79109480d6dd41a644d583f4b7f7
zwf <zwf19980423@gmail.com>
2021-01-24 12:56:16 +0800

    Productcontroller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,12 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-const router = express.Router();
-import Product from '../models/productModel.js';
 
-//@description fetch all products
-//@router Get /api/products
-//@access public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-//@description fetch single product
-//@router Get /api/products/:id
-//@access public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('product not found');
-    }
-  })
-);
+const router = express.Router();
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
+
+router.route('/').get(getProducts);
+
+router.route('/:id').get(getProductById);
 export default router;
