f8d8b1ee9685161c491fa458fc6701c189748dd7
zwf <zwf19980423@gmail.com>
2021-02-06 12:44:24 +0800

    admin update user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentSceen from './Screens/PaymentScreen';
 import PlaceOrderScreen from './Screens/PlaceOrderScreen';
 import OrderScreen from './Screens/OrderScreen';
 import UserListScreen from './Screens/UserListScreen';
+import UserEditScreen from './Screens/UserEditScreen';
 const App = () => {
   return (
     <Router>
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
