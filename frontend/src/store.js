a6071e61072e10653351136afb9ab6652445f790
zwf <zwf19980423@gmail.com>
2021-02-08 19:05:40 +0800

    add comment

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
   producDeleteReducer,
   producCreateReducer,
   producUpdateReducer,
+  productReviewCreateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -33,6 +34,7 @@ const reducer = combineReducers({
   productDelete: producDeleteReducer,
   productCreate: producCreateReducer,
   productUpdate: producUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
