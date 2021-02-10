086ccc4c274d44b069f79a8da4f6803d7b625fe8
zwf <zwf19980423@gmail.com>
2021-02-10 12:27:05 +0800

    last comnmit

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,6 @@ app.use((req, res, next) => {
   console.log(req.originalUrl);
   next();
 });
-app.get('/', (req, res) => res.send('API is running...'));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -37,6 +36,14 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => res.send('API is running...'));
+}
 
 app.use(notFound);
 app.use(errorHandler);
